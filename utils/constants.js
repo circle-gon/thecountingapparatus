15fbbd425d14a13a1aa4b4a85bafddd8112a8e1c
Glitch (delicate-numerous-manatee) <none>
2022-10-12 14:32:16 +0000

    💅🌶 Checkpoint ./utils/constants.js:32659443/1249

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,10 @@
+import { basicCount } from "./factions/count.js"
+import { treeCount } from "./factions/tree.js"
+import { letterCount } from "./factions/letter.js"
+import { xxCount } from "./factions/xx.js"
+import { onesCount } from "./factions/ones.js"
+import { factorialCount } from "./factions/factorial.js"
+
 const RULES = {
   Classic: [
     "1) Count as high as possible in increments of Effective X.",
@@ -26,5 +33,34 @@ const RULES = {
   ]
 }
 const RULES_ABRIDGED = {
-  Classic:
+  Classic: [
+    "Standard counting",
+    "Benefits: Unbans functions, yields Letter stock",
+    "Current Count"+basicCount.count,
+    "Current Milestones"+basicCount.milestones,
+  ],
+  
+  Tree: [
+    "Slowmode counting",
+    "Benefits: Unbans functions, increases Character Limit of messages",
+    "Current Count"+basicCount.count,
+    "Current Milestones"+basicCount.milestones,
+  ],
+  
+  Letter: [
+    "Counting with Letters",
+    
+  ],
+  
+  X_X: [
+    
+  ],
+  
+  Ones: [
+    
+  ],
+  
+  Factorial: [
+    
+  ]
 }
