a18ede497ca4e214452a690e58da61fee98a0821
Glitch (delicate-numerous-manatee) <none>
2022-10-12 21:57:41 +0000

    🎍💧 Checkpoint ./functions/functionList.js:9785301/1152:25758330/681:49619082/325

--- functions/functionList.js
+++ functions/functionList.js
@@ -4,14 +4,39 @@ import {gamma} from './factorial.js';
 //Subclasses
   class NChooseR extends Function {
     constructor (name, unlock, syntax, input1, input2){
-      super(name, unlock, syntax, gamma(input1+1)/(gamma(input2+1)*gamma(input1-input2+1))); //n!/ (r! (n−r)!)
+      super(name, unlock, syntax, (input1, input2)=>gamma(input1+1)/(gamma(input2+1)*gamma(input1-input2+1))); //n!/ (r! (n−r)!)
     }
   }
 
   class Logarithm extends Function {
-    constructor (name, unlock, syntax, base, input){
-      super(name, unlock, syntax, Math.log(input)/Math.log(base));
+    constructor (name, unlock, syntax, base){
+      super(name, unlock, syntax, (input)=>Math.log(input)/Math.log(base));
+    }
+  }
+  class SingleNumSeq extends Function {
+    constructor (name, unlock, syntax, num) {
+      super(name, unlock, syntax, (input)=>num)
     }
   }
 //Functions (organize by unlock)
-  
+  const FUNCTIONS = {
+    "A": new Function("Ackermann Function", "A", "A(x,y)", function ),
+    "LN": new Logarithm("Natural Log", "LN", "ln(x)", Math.E),
+    "LOG10": new Logarithm("Logarithm", "LOG", "log10(x)", 10),
+    "LOG2": new Logarithm("Binary Logarithm", "BL", "log2(x)", 2),
+    "CHOOSE": new NChooseR("nCr", "CHOOSE", "xCy"),
+    "ZERO": new SingleNumSeq("All Zeros Sequence", "ZERO", "zero(x)", 0),
+    "ONE": new SingleNumSeq("All Ones Sequence", "ONE","one(x)",1),
+    "TWO": new SingleNumSeq("All Twos Sequence", "TWO","two(x)",2),
+    "THREE": new SingleNumSeq("All Threes Sequence", "THREE","three(x)",3),
+    "FOUR": new SingleNumSeq("All Fours Sequence", "FOUR","four(x)",4),
+    "FIVE": new SingleNumSeq("All Fives Sequence", "FIVE","five(x)",5),
+    "SIX": new SingleNumSeq("All Sixes Sequence", "SIX","six(x)",6),
+    "SEVEN": new SingleNumSeq("All Sevens Sequence", "SEVEN","seven(x)",7),
+    "EIGHT": new SingleNumSeq("All Eights Sequence", "EIGHT","eight(x)",8),
+    "NINE": new SingleNumSeq("All Nines Sequence", "NINE","nine(x)",9),
+    "TEN": new SingleNumSeq("All Tens Sequence", "TEN","ten(x)",10),
+    "ELEVEN": new SingleNumSeq("All 11 Albanias", "ELEVEN", ":flag_al:(x)", 11),
+    "TWELVE": new SingleNumSeq("Those 12 bees", "TWELVE", ":bee:(x)", 12),
+  }
+// trole !
