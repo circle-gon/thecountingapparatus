7ecc4d991ac3ff416cb74e5a303f7a8f25964420
Glitch (delicate-numerous-manatee) <none>
2022-10-12 14:08:40 +0000

    🕴😘 Checkpoint ./factions/letter.js:32659443/15 ./factions/xx.js:32659443/1045 ./utils/mechanics.js:32659443/1983

--- factions/xx.js
+++ factions/xx.js
@@ -1,37 +1,7 @@
 import { FactionBase } from "./faction.js";
 
-/*function getEffectiveMilestones(){
-  let effectiveMilestones = 0;
-  for (const value of Object.values(globalInfo.factions)) {
-    if(["Letter", "Factorial", "Matrix", "Function"].includes(value.name))
-      effectiveMilestones -= value.milestone;
-  	else effectiveMilestones += value.milestone;
-  }
-  return effectiveMilestones
-}
-function getTotalMilestones(){
-  let totalMilestones = 0;
-  for (const value of Object.values(globalInfo.factions)) {
-  	totalMilestones += value.milestone;
-  }
-  return totalMilestones
-}
-function updateMilestoneReduction() {
-    //updates milestone scaling reduction on each milestone
-	let effectiveMilestones = getEffectiveMilestones()
-  milestoneReduction = Math.pow(1/xxCount.logProd, Math.max(1/(effectiveMilestones+1),1));
-  document.getElementById("milestoneReductionText").innerHTML = "Milestone reduction: " + milestoneReduction.toFixed(2);
-  for (const value of Object.values(globalInfo.factions)) {
-  	value.checkMilestoneReq();
-  }
-}
-
-let milestoneReduction = 1;*/
-
 class XxFaction extends FactionBase {
   constructor() {
-    // testing testing
-    //okay?
     super("X X", (x) => Math.pow(x, Math.pow(x, this.milestoneReduction)));
     this.challenges = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     this.rawX = 1;
---
+++ utils/mechanics.js
@@ -0,0 +1,17 @@
+export function getEffectiveMilestones(){
+
+}
+
+export function getTotalMilestones(){
+
+}
+
+export function updateMilestoneReduction() {
+
+}
+
+export function getAverage(){
+  
+}
+
+export function 
