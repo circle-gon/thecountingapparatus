b2055ee0812fca72a049951ebdc309a8c1b19a8e
Glitch (delicate-numerous-manatee) <none>
2022-10-12 14:44:16 +0000

    👱👯 Checkpoint ./utils/constants.js:32659443/671

--- utils/constants.js
+++ utils/constants.js
@@ -9,11 +9,14 @@ const RULES = {
   Classic: [
     "1) Count as high as possible in increments of Effective X.",
     "2) Milestones occur every power of 10 and yield unbans & letter stock.",
-    "3) Type \"unban <function_name> to unban a function (one function at a time). This counts as a post."
+    "3) Type \"unban <function_name>\" to unban a function (one function at a time). This counts as a post."
     ],
   
   Tree: [
-    
+    "1) Count as high as possible with a 1 day slowmode", //1 day should be modifiable
+    "2) Milestones occur every perfect square and yield unbans.",
+    "3) Grid size "
+    "4) Type \"unban <function_name>\" to unban a function (one function at a time). This counts as a post."
   ],
   
   Letter: [
@@ -32,6 +35,7 @@ const RULES = {
     
   ]
 }
+
 const RULES_ABRIDGED = {
   Classic: [
     "Standard counting",
@@ -43,12 +47,15 @@ const RULES_ABRIDGED = {
   Tree: [
     "Slowmode counting",
     "Benefits: Unbans functions, increases Character Limit of messages",
-    "Current Count"+basicCount.count,
-    "Current Milestones"+basicCount.milestones,
+    "Current Count"+treeCount.count,
+    "Current Milestones"+treeCount.milestones,
   ],
   
   Letter: [
     "Counting with Letters",
+    "Benefits: Reduces effective milestones, Unlocks functions",
+    "Current Count"+letterCount.count,
+    "Current Milestones"+letterCount.milestones,
     
   ],
   
