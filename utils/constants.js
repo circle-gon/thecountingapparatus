cbfac22e57d9a6a39672427562d217cbb89e1fcb
Glitch (delicate-numerous-manatee) <none>
2022-10-12 14:20:18 +0000

    🕰🍃 Checkpoint ./utils/mechanics.js:32659443/18 ./utils/constants.js:32659443/834

---
+++ utils/constants.js
@@ -0,0 +1,30 @@
+const RULES = {
+  Classic: [
+    "1) Count as high as possible in increments of Effective X.",
+    "2) Milestones occur every power of 10 and yield unbans & letter stock.",
+    "3) Type \"unban <function_name> to unban a function (one function at a time). This counts as a post."
+    ],
+  
+  Tree: [
+    
+  ],
+  
+  Letter: [
+    
+  ],
+  
+  X_X: [
+    
+  ],
+  
+  Ones: [
+    
+  ],
+  
+  Factorial: [
+    
+  ]
+}
+const RULES_ABRIDGED = {
+  Classic:
+}
--- utils/mechanics.js
+++ utils/mechanics.js
@@ -13,5 +13,3 @@ export function updateMilestoneReduction() {
 export function getAverage(){
   
 }
-
-export function 
