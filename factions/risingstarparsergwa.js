786a0df2f78dae3440df148afc68c4165773ba16
Glitch (delicate-numerous-manatee) <none>
2022-10-16 18:32:36 +0000

    🍥💺 Checkpoint ./factions/risingstarparsergwa.js:15796239/1056 ./text/groups.js:37514084/2430 ./group-style.css:37514084/3163

--- factions/risingstarparsergwa.js
+++ factions/risingstarparsergwa.js
@@ -1,3 +1,12 @@
+import {
+  Functions,
+  // Operators,
+  // Left,
+  // Right,
+  // Wrap,
+  // Bin,
+} from "../functions/functionClass.js";
+
 function parse(str) {
   // ASSUMES FULL BRACKETING
   // ALSO ASSUMES EVERYTHING IS A FUNCTION (very easy if things are suggested/selected from a list client side)
@@ -6,10 +15,12 @@ function parse(str) {
   const splitStr = str.split('/[()]+/');
   // look for literals (the base case)
   let literals = [];
+  let literalsIndexes = [];
   for (let i = 0; i < splitStr.length(); i++) {
     // check if literal and add to list - maybe have it add index as well?
     if ('/[0123456789]/'.test(splitStr[i])) {
-      literals.push([splitStr[i], i]);
+      literals.push(splitStr[i]);
+      literalsIndexes.push(i);
     }
   }
   for (let i = 0; i < literals.length(); i++) {
@@ -18,5 +29,21 @@ function parse(str) {
     let leftFunc = splitStr[index - 1];
     // rightFunc should be either nonexistant or a comma
     let rightFunc = splitStr[index + 1]; // stop this checking oob
+    if (leftFunc == ',') {
+      // ignore it
+      continue;
+    }
+    // find number of arguments (work out arbitrary args later, may need to keep brackets in split)
+    let expectedArgs = Functions[leftFunc].expectedArgs;
+    let args = [];
+    for (let j = 0; j < expectedArgs.length(); j++) {
+      // has arg been calculated to literal already
+      if (splitStr[index + 1] == ',' && literalsIndexes.includes(index + 2)) {
+        args.push(literals[index + 2]);
+      }
+    }
+    if (args.length == expectedArgs) {
+      let result = Functions[leftFunc].evaluate(args);
+    }
   }
 }
--- text/groups.js
+++ text/groups.js
@@ -22,83 +22,22 @@ class TextGroupDisplay extends HTMLElement {
     const name = this.getAttribute("name")
     this.textGroup = textGroups[name]
     const wrapper = ce("div")
-    const style = ce("style")
+    const style = ce("link")
     // THIS IS TESTING NOT THE FINAL PRODUCT
     wrapper.innerHTML = `
-  <div class="container">
-    <input type="checkbox"
-           id="accordion-1" 
-           class="accordion-control">
-    <label for="accordion-1"
-           class="accordion-label">Click to</label>
-    <div class="accordion-content">
+      <div class="container">
+        <input type="checkbox"
+           id="check" 
+           class="control">
+    <label for="check"
+           class="label">Click to</label>
+    <div class="content">
       This is collapsible content
     </div>
 </div>
     `
-    style.innerHTML = `
-.accordion-label {
-  cursor: pointer;
-  display: block;
-  user-select: none;
-  color: #fff;
-}
-
-.container {
-  background: #861c7e;
-  padding: 20px;
-  width: fit-content;
-  border-radius: 20px;
-}
-
-.accordion-content {
-  /* Height animation:
-  only really works well if the height is close to content - too high and the animation still goes to the max-height number, messing up the timing
-  For widely variable height content, best not to animate. Switch display attribute instead */
-  max-height: 0;
-  overflow: hidden;
-  transition: max-height 0.2s;
-  background-size: 260px;
-  background-repeat: no-repeat;
-  background-position: center;
-  color: #fff;
-}
-
-.accordion-control {
-  display: none;
-}
-.accordion-control + label {
-  position: relative;
-  height: 40px;
-  margin-left: 20px;
-  padding-left: 5px;
-  display: block;
-  font-family: arial;
-  font-size: 1em;
-  line-height: 1em;
-  z-index: 0;
-}
-.accordion-control + label:before, .accordion-control + label:after {
-  content: "";
-  position: absolute;
-}
-.accordion-control + label:before {
-  left: -19px;
-  top: 3px;
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-width: 10px 0 10px 16px;
-  border-color: transparent transparent transparent #fff;
-  transition: all 0.2s ease-in;
-}
-.accordion-control:checked + label:before {
-  transform: rotate(90deg);
-}
-.accordion-control:checked + label + .accordion-content {
-  max-height: 350px;
-}
-    `
+    style.rel = "stylesheet"
+    style.href = "group-style.css"
     this.shadowRoot.append(wrapper, style)
   }
 }
