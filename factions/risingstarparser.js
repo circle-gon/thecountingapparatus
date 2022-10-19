ac97f014d1909a7ed194eba584b71b2136f8b3e9
Glitch (delicate-numerous-manatee) <none>
2022-10-19 00:17:13 +0000

    🍯🌁 Checkpoint ./factions/risingstarparser.js:15796239/2213

--- factions/risingstarparser.js
+++ factions/risingstarparser.js
@@ -54,7 +54,6 @@ export function parse2(str) {
           throw new ParserError("no operation found but not finished");
         }
       }
-      let collapseAlgo = null;
       // left existence
       let foundFunction = null;
       let foundLength = 0;
@@ -81,7 +80,6 @@ export function parse2(str) {
         }
       }
       if (foundFunction !== null) {
-        collapseAlgo = "left";
         console.log(foundFunction);
         // look for other args
         const expectedArgs = foundFunction.syntax.split(",");
@@ -283,73 +281,101 @@ export function parse2(str) {
   return literals[0];
 }
 
-function parse(str) {
-  // ASSUMES FULL BRACKETING
-  // ALSO ASSUMES EVERYTHING IS A FUNCTION (very easy if things are suggested/selected from a list client side)
-  // ALSO ASSUMES MORE STUFF AND ISN'T FINISHED
-  // ALSO GWA
-  
-  // split into a full arrawy of just functions and their arguments
-  const splitStr = str.split(/[()]+/);
-  // look for literals (the base case)
-  let literals = [];
-  let literalsIndexes = [];
-  for (let i = 0; i < splitStr.length(); i++) {
-    // check if literal and add to list - maybe have it add index as well?
-    if (/[0123456789]/.test(splitStr[i])) {
-      literals.push(splitStr[i]);
-      literalsIndexes.push(i);
+function findFunction(str, startIndex, direction, ignoreBrackets, delimiterArgs) {
+  let foundFunction = null;
+  let foundLength = 0;
+  for (let distanceLeft = 1; distanceLeft <= startIndex; distanceLeft++) {
+    // find if there's a function with fitting syntax
+    // ignore brackets if no function has been found yet
+    let subString = str.substring(startIndex - distanceLeft, startIndex);
+    if (foundFunction === null && ignoreBrackets) {
+      subString = subString.replace(/[()]/g, '');
+    }
+    const actualFunc = Object.values(FUNCTIONS).find( function (i) {
+      // possibly split up operators to prioritize
+      const syntaxSub = i.syntax.substring(0, i.syntax.indexOf("("));
+      if (syntaxSub === '') {
+        return false;
+      }
+      return i.syntax === subString || syntaxSub === subString;
+    }
+    );
+    // check actualFunc exists, dunno how tho, so just assume it isn't if it's locked
+    if (actualFunc !== undefined && actualFunc.isUnlocked) {
+      foundFunction = actualFunc;
+      foundLength = distanceLeft;
     }
   }
-  while (splitStr.length() > 1) {
-    for (let i = 0; i < literals.length(); i++) {
-      let value = literals[i][0];
-      let index = literals[i][1];
-      let leftFunc = splitStr[index - 1];
-      // rightFunc should be either nonexistant or a comma
-      //let rightFunc = splitStr[index + 1]; // stop this checking oob
-      if (leftFunc == ',') {
-        // ignore it
-        continue;
-      }
-      // find number of arguments (work out arbitrary args later, may need to keep brackets in split)
-      if (!(leftFunc in Functions) || !Functions[leftFunc].unlocked) {
-        // tell them they're a fool
-      }
-      if (Functions[leftFunc].isBanned || Functions[leftFunc].isStunned) {
-        // special stuff
-      }
-      let expectedArgs = Functions[leftFunc].expectedArgs;
-      let args = [];
-      let argsIndexes = [];
-      for (let j = 0; j < expectedArgs.length(); j++) {
-        // has arg been calculated to literal already
-        if (splitStr[index + 1] == ',' && literalsIndexes.includes(index + 2)) {
-          args.push(literals[index + 2]);
-          argsIndexes.push(literalsIndexes.indexOf(index + 2));
-        }
-      }
-      if (args.length == expectedArgs.length()) {
-        let result = Functions[leftFunc].evaluate(args);
-        // update literals lists
-        for (let k = 0; k < expectedArgs.length(); k++) {
-          literals[argsIndexes[k]] = 'test'; // to avoid resizing while deleting
-          literalsIndexes[argsIndexes[k]] = 'test';
-        }
-        literals.removeAll('test');
-        literalsIndexes.removeAll('test');
-        literals.push(result);
-        literalsIndexes.push(index - 1);
-        // update splitStr
-        splitStr[index - 1] = result
-          // remove calculated stuff
-        for (let j = 0; j < expectedArgs.length() * 2 - 1; j++) {
-          splitStr.removeAt(index);
-        }
-        // reset the literals search
-        break;
-      }
-    }
-  }
-  return splitStr[0]; // gwa
+  return [foundFunction, foundLength];
 }
+
+// function parse(str) {
+//   // ASSUMES FULL BRACKETING
+//   // ALSO ASSUMES EVERYTHING IS A FUNCTION (very easy if things are suggested/selected from a list client side)
+//   // ALSO ASSUMES MORE STUFF AND ISN'T FINISHED
+//   // ALSO GWA
+  
+//   // split into a full arrawy of just functions and their arguments
+//   const splitStr = str.split(/[()]+/);
+//   // look for literals (the base case)
+//   let literals = [];
+//   let literalsIndexes = [];
+//   for (let i = 0; i < splitStr.length(); i++) {
+//     // check if literal and add to list - maybe have it add index as well?
+//     if (/[0123456789]/.test(splitStr[i])) {
+//       literals.push(splitStr[i]);
+//       literalsIndexes.push(i);
+//     }
+//   }
+//   while (splitStr.length() > 1) {
+//     for (let i = 0; i < literals.length(); i++) {
+//       let value = literals[i][0];
+//       let index = literals[i][1];
+//       let leftFunc = splitStr[index - 1];
+//       // rightFunc should be either nonexistant or a comma
+//       //let rightFunc = splitStr[index + 1]; // stop this checking oob
+//       if (leftFunc == ',') {
+//         // ignore it
+//         continue;
+//       }
+//       // find number of arguments (work out arbitrary args later, may need to keep brackets in split)
+//       if (!(leftFunc in Functions) || !Functions[leftFunc].unlocked) {
+//         // tell them they're a fool
+//       }
+//       if (Functions[leftFunc].isBanned || Functions[leftFunc].isStunned) {
+//         // special stuff
+//       }
+//       let expectedArgs = Functions[leftFunc].expectedArgs;
+//       let args = [];
+//       let argsIndexes = [];
+//       for (let j = 0; j < expectedArgs.length(); j++) {
+//         // has arg been calculated to literal already
+//         if (splitStr[index + 1] == ',' && literalsIndexes.includes(index + 2)) {
+//           args.push(literals[index + 2]);
+//           argsIndexes.push(literalsIndexes.indexOf(index + 2));
+//         }
+//       }
+//       if (args.length == expectedArgs.length()) {
+//         let result = Functions[leftFunc].evaluate(args);
+//         // update literals lists
+//         for (let k = 0; k < expectedArgs.length(); k++) {
+//           literals[argsIndexes[k]] = 'test'; // to avoid resizing while deleting
+//           literalsIndexes[argsIndexes[k]] = 'test';
+//         }
+//         literals.removeAll('test');
+//         literalsIndexes.removeAll('test');
+//         literals.push(result);
+//         literalsIndexes.push(index - 1);
+//         // update splitStr
+//         splitStr[index - 1] = result
+//           // remove calculated stuff
+//         for (let j = 0; j < expectedArgs.length() * 2 - 1; j++) {
+//           splitStr.removeAt(index);
+//         }
+//         // reset the literals search
+//         break;
+//       }
+//     }
+//   }
+//   return splitStr[0]; // gwa
+// }
